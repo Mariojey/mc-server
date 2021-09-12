01483782fd84d2e5513d697f88d10ca78c835ad2
Mariojey <marios.yaceg@gmail.com>
2021-09-12 12:17:49 +0200

    Utworzenie pliku index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="widt=device-width, initial-scale=1">
+    <title>MC - Server</title>
+  </head>
+  <body>
+
+  </body>
+</html>
