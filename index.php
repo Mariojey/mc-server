ce09377ebefcb27c6c1b6d4b211d99e898590cbf
Mariojey <marios.yaceg@gmail.com>
2021-09-12 13:07:23 +0200

    Searchbar w html-u

--- index.php
+++ index.php
@@ -4,8 +4,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="widt=device-width, initial-scale=1">
     <title>MC - Server</title>
+    <script src="https://kit.fontawesome.com/e93d8a5c79.js" crossorigin="anonymous"></script>
   </head>
   <body>
-
+    <form class="searchbar" action="index.html" method="post">
+      <label for="search">Search</label>
+      <input type="text" name="search" placeholder="Wpisz swój minecraftowy nick...">
+      <button type="submit" name="submit">
+        <i class="fas fa-search"></i>
+      </button>
+    </form>
   </body>
 </html>
